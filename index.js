96d014fed1a90602f43a0c61e4a7d7e8114705b3
Ziv Weissman <ziv@ucsb.edu>
2022-10-14 23:35:36 -0700

    implemented redis to utillize pub/sub channels rather than originally intended peer to peer connections and broadcast, synchronization, and late startup functionallity

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const request = require('request');
 const bodyParser = require('body-parser');
 const Blockchain = require('./blockchain');
+const PubSub = require('./pubsub');
 
 const app = express();
 const blockchain = new Blockchain();
+const pubsub = new PubSub({ blockchain });
+
+const DEFAULT_PORT = 3000;
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
+
 
 //configuring node to rely on this dependency
 //bodyParser parses requests bodies as objects before the handlers
@@ -24,15 +31,40 @@ app.post('/api/mine', (req, res) => {
     //adding the block to the local chain
     blockchain.addBlock({ data });
 
+    //broadcasting the local chain for others to authorize
+    pubsub.broadcastChain();
+
     //sends the user back to view the chain data
     res.redirect('/api/blocks');
 });
 
+const syncChains = () => {
+    request({ url: `${ROOT_NODE_ADDRESS}/api/blocks` }, (error, response, body) => {
+        if(!error && response.statusCode === 200) {
+            const rootChain = JSON.parse(body);
+
+            console.log('replace chain on a sync with', rootChain);
+            blockchain.replaceChain(rootChain);
+        }
+    });
+};
+
+let PEER_PORT;
+
+if (process.env.GENERATE_PEER_PORT === 'true') {
+    PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random()* 1000);
+}
+
 //TODO: research ports
 //just using default reccomended 3000 for time being
-const PORT = 3000
+const PORT = PEER_PORT || DEFAULT_PORT;
 app.listen(PORT, () => {
     console.log(`listening at localhost:${PORT}`);
+
+    //preventing a redundant call from the rootnode to sync to itself
+    if (PORT !== DEFAULT_PORT) {
+        syncChains();
+    }
 });
 
 //
